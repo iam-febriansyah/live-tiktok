a38f9fac7763abf8cdcc669337e0f45ab38bd05f
Febriansyah Syahyadi <s.febriansyah.s@musashi.co.id>
2023-06-09 16:37:01 +0700

    jwt

--- app/middleware.js
+++ app/middleware.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = {
   jwtToSession: async (req, res, next) => {
     if (req.session.user === null || req.session.user === undefined) {
-      var token = req.cookies.auth;
+      var token = req.cookies?.auth;
       jwt.verify(token, process.env.JWT_KEY, function (err, token_data) {
         if (err) {
           next();
