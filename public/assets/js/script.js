26f9d5d2ee10e67ec77b4c40da20a5a1eceefa6b
Febriansyah Syahyadi <s.febriansyah.s@musashi.co.id>
2023-06-09 16:33:24 +0700

    disable csrf

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -93,7 +93,7 @@ async function ajaxPost(url, dataPost, btnId) {
     return await Promise.resolve(
       $.ajax({
         url: url,
-        headers: header,
+        // headers: header,
         type: "post",
         dataType: "json",
         data: dataPost,
@@ -159,7 +159,7 @@ async function ajaxPostWIthList(url, dataPost, btnId) {
     return await Promise.resolve(
       $.ajax({
           url: url,
-          headers: header,
+          // headers: header,
           type: "post",
           dataType: "json",
           data: JSON.stringify(dataPost),
