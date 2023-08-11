317105ce90bb59b79f932b178f2d353ecd8c6698
Febriansyah <febriansyah032@gmail.com>
2023-08-11 20:59:08 +0700

    Update

--- app/live.js
+++ app/live.js
@@ -49,6 +49,7 @@ async function liveStream(acc, io) {
         const room_id = licenses[i].room_id;
         socket.emitGift(io, data, tiktok_username, license);
         var json_data = convertGift(data);
+        json_data.createdAt = help.dateTimeNow();
         if (room_id) {
           worker.worker_insertGift({ room_id: room_id, json_data: json_data, created_by: license });
         }
